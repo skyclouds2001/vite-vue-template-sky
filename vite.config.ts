e2523645aa6677c6117a57a9dcf41517b9166ba3
skyclouds2001 <skyclouds2001@163.com>
2022-09-04 01:47:10 +0800

    feat: config unplugin-element-plus/vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import ElementPlus from 'unplugin-element-plus/vite';
 import Icons from 'unplugin-icons/vite';
 import IconsResolver from 'unplugin-icons/resolver';
 import Inspect from 'vite-plugin-inspect';
@@ -70,5 +71,12 @@ export default defineConfig({
       autoInstall: true,
     }),
     Inspect(),
+    ElementPlus({
+      useSource: false,
+      lib: 'element-plus',
+      format: 'esm',
+      prefix: 'El',
+      defaultLocale: 'zh-cn',
+    }),
   ],
 });
