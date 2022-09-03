bc6ab2d331d21f3a8e70c790737a9b4d7f16b7f7
skyclouds2001 <skyclouds2001@163.com>
2022-09-04 00:18:37 +0800

    feat: config auto import component plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   resolve: {
@@ -9,6 +12,12 @@ export default defineConfig({
     },
   },
   plugins: [
-    vue()
-  ]
-})
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+});
