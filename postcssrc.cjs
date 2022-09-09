f32603a677bb0e91062862e90ae9433c7ae36bcf
skyclouds2001 <skyclouds2001@163.com>
2022-09-09 14:43:07 +0800

    feat: config sugarss postcss-preset-env postcss-preset-env

--- postcssrc.cjs
+++ postcssrc.cjs
@@ -1,9 +1,13 @@
 const autoprefixer = require('autoprefixer')
+const postcssPresetEnv = require('postcss-preset-env')
+const postcssImport = require('postcss-import')
 
 module.exports = (ctx) => ({
   parser: ctx.parser ? 'sugarss' : false,
   map: ctx.env === 'development' ? ctx.map : false,
   plugins: {
-    autoprefixer
+    postcssImport,
+    autoprefixer,
+    postcssPresetEnv,
   },
 })
