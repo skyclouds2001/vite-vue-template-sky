10b646d497df47590133274db9485d6228dc89ef
skyclouds2001 <skyclouds2001@163.com>
2022-09-10 15:44:26 +0800

    feat: remove & rename base css file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './styles/main.css'
 import App from './App.vue'
 import router from '@/router/router'
 import store from '@/store/store'
