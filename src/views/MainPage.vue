a682a43abbccfbdb886fbe5dc3ebfe229515198a
skyclouds2001 <skyclouds2001@163.com>
2022-09-30 21:52:04 +0800

    refactor(*): little change

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -5,7 +5,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  name: 'MainPage',
+})
 </script>
 
 <style></style>
