fba70f201a4c15b9e92c6b7f0cf31293dd6ac917
skyclouds2001 <skyclouds2001@163.com>
2023-01-05 21:04:51 +0800

    build: remove v1 file

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './styles/main.css'
-import App from './App.vue'
-import router from '@/router/router'
-import store from '@/store/store'
-
-createApp(App).use(router).use(store).mount('#app')
