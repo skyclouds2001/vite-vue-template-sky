e1fd0facd10146155ade9723e824e24d4ca160d1
skyclouds2001 <skyclouds2001@163.com>
2023-01-06 13:47:34 +0800

    build: use legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 import ElementPlus from 'unplugin-element-plus/vite'
 
 export default defineConfig({
-  plugins: [vue(), ElementPlus()],
+  plugins: [vue(), legacy(), ElementPlus()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/')
