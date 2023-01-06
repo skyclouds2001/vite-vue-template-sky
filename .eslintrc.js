40eac8292008f41148c785e54627c516a580eb3f
skyclouds2001 <skyclouds2001@163.com>
2023-01-06 20:56:30 +0800

    build: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    commonjs: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  },
+  ignorePatterns: [
+    'vite.config.ts',
+    '.eslintrc.js'
+  ]
+}
