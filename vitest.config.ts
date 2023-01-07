75e2766fdce59723d4c4ad5211f67af74bbdfe68
skyclouds2001 <skyclouds2001@163.com>
2023-01-07 23:41:44 +0800

    build: add vue-jsx support

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import legacy from '@vitejs/plugin-legacy'
 import ElementPlus from 'unplugin-element-plus/vite'
 
 export default defineConfig({
-  plugins: [vue(), legacy(), ElementPlus()],
+  plugins: [vue(), vueJsx(), legacy(), ElementPlus()],
   css: {
     postcss: 'postcss.config.js',
     devSourcemap: true,
