d8551dda705a1ce8707d4c24e0141495582b230e
skyclouds2001 <skyclouds2001@163.com>
2023-02-04 00:46:01 +0800

    build: update stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,47 @@
 module.exports = {
-  customSyntax: 'postcss-html',
-  extends: ['stylelint-config-standard', 'stylelint-config-standard-vue', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-prettier/recommended'],
   rules: {},
+  plugins: ['stylelint-order'],
+  overrides: [
+    {
+      files: ['**/*.md'],
+      customSyntax: 'postcss-markdown',
+    },
+    {
+      files: ['**/*.html'],
+      extends: ['stylelint-config-html'],
+    },
+    {
+      files: ['**/*.vue'],
+      extends: ['stylelint-config-standard-vue'],
+    },
+    {
+      files: ['**/*.sass'],
+      customSyntax: 'postcss-sass',
+    },
+    {
+      files: ['**/*.scss'],
+      extends: [
+        'stylelint-config-standard-scss',
+        'stylelint-config-prettier-scss',
+      ],
+    },
+    {
+      files: ['**/*.less'],
+      extends: ['stylelint-config-recommended-less'],
+      customSyntax: 'postcss-less',
+    },
+    {
+      files: ['**/*.styl', '**/*.stylus'],
+      extends: ['stylelint-stylus/standard'],
+    },
+    {
+      files: ['**/*.sss'],
+      customSyntax: 'sugarss',
+    },
+    {
+      files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+      customSyntax: '@stylelint/postcss-css-in-js',
+    },
+  ],
 }
