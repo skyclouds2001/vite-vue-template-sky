e17e549df55e3c18321a0432e16c87033184c68d
skyclouds2001 <skyclouds2001@163.com>
2023-02-06 19:32:58 +0800

    build: run lint of config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}",
-  ],
+  content: ['./index.html', './src/**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/line-clamp'), require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
