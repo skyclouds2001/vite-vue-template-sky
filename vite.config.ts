a497248b045782a8a10e99092b5cb473ae42d5b2
skyclouds2001 <skyclouds2001@163.com>
2023-02-06 18:28:59 +0800

    build: add vite-eslint and vite-stylelint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import legacy from '@vitejs/plugin-legacy'
+import eslint from 'vite-plugin-eslint'
+import stylelint from 'vite-plugin-stylelint'
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(), legacy()],
+  plugins: [vue(), vueJsx(), legacy(), eslint(), stylelint()],
   css: {
     postcss: 'postcss.config.js',
     devSourcemap: true,
