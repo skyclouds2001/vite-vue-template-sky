e42d4b184469d68de5bbe6ff888987c0dee7acbf
skyclouds2001 <skyclouds2001@163.com>
2023-02-06 22:06:40 +0800

    build: remove playwright test

--- tests/e2e/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('homepage details', async ({ page }) => {
-  await page.goto('/')
-
-  await expect(page).toHaveTitle('Vite-Vue-Template-Sky')
-})
