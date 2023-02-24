4d501bbe84c801a2e431268d3d38738413a1cceb
skyclouds2001 <skyclouds2001@163.com>
2023-02-24 21:33:32 +0800

    feat: update alias support of ~ as root

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src/'),
+      '~': __dirname,
+      '@': path.resolve(__dirname, './src/'),
     },
   },
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src/'),
+      '~': __dirname,
+      '@': path.resolve(__dirname, './src/'),
     },
   },
   test: {
