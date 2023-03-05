abadbe81a7ad687925b633a143d527f4761f6621
skyclouds2001 <skyclouds2001@163.com>
2023-03-05 16:49:37 +0800

    feat: add element-plus icon example

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ElButton } from 'element-plus'
+import { ElButton, ElIcon } from 'element-plus'
+import { User } from '@element-plus/icons-vue'
 import { useMainStore } from '@/store'
 
 const store = useMainStore()
@@ -15,6 +16,9 @@ const decrease = () => {
 
 <template>
   <div class="w-10">main</div>
+  <div>
+    <el-icon><User /></el-icon>
+  </div>
   <el-button type="primary">Default</el-button>
   <div>
     <span @click="decrease">-</span>
