0706b35588cff08b2af672fd92332a3b8f0abe8c
skyclouds2001 <skyclouds2001@163.com>
2023-06-28 22:20:33 +0800

    build: use checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,9 @@ import { viteMockServe as mock } from 'vite-plugin-mock'
 import mkcert from 'vite-plugin-mkcert'
 import svgLoader from 'vite-svg-loader'
 import ElementPlus from 'unplugin-element-plus/vite'
-import eslint from 'vite-plugin-eslint'
-import stylelint from 'vite-plugin-stylelint'
 import visualizer from 'rollup-plugin-visualizer'
 import inspect from 'vite-plugin-inspect'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
   plugins: [
@@ -21,14 +20,6 @@ export default defineConfig({
     mkcert(),
     svgLoader(),
     ElementPlus({}),
-    eslint({
-      cache: true,
-      cacheLocation: 'node_modules/.eslint/.eslintcache',
-    }),
-    stylelint({
-      cache: true,
-      cacheLocation: 'node_modules/.stylelint/.stylelintcache',
-    }),
     visualizer({
       filename: 'report.html',
       title: 'report',
@@ -37,6 +28,15 @@ export default defineConfig({
       brotliSize: true,
     }),
     inspect(),
+    checker({
+      vueTsc: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx,vue}"',
+      },
+      stylelint: {
+        lintCommand: 'stylelint ./src/**/*.{vue,css,sass,scss,less,styl,stylus}',
+      },
+    }),
   ],
   resolve: {
     alias: {
