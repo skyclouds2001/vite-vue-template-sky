0fb18def5ecd5d939782637355624d35dbcea41f
skyclouds2001 <skyclouds2001@163.com>
2024-02-04 22:13:31 +0800

    build: update stylelint configuration

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,5 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
-  plugins: [],
   rules: {
     'at-rule-no-unknown': [
       true,
@@ -9,16 +7,23 @@ export default {
         ignoreAtRules: ['tailwind', 'layer', 'apply', 'variants', 'responsive', 'screen'],
       },
     ],
-    'no-empty-source': null,
   },
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  plugins: [],
   overrides: [
-    {
-      files: ['**/*.vue'],
-      extends: ['stylelint-config-standard-vue'],
-    },
     {
       files: ['**/*.scss'],
       extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
     },
+    {
+      files: ['**/*.vue'],
+      extends: ['stylelint-config-standard-vue'],
+      rules: {
+        'no-empty-source': null,
+      },
+    },
   ],
+  allowEmptyInput: true,
+  cache: true,
+  fix: true,
 }
