1c0193631604cc1b58096e0ad61ecc6faa59fa44
skyclouds2001 <skyclouds2001@163.com>
2024-02-04 21:51:45 +0800

    build: change lint-staged configuration file to ESM format

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   '*.{js,jsx,ts,tsx,vue}': ['eslint --fix'],
   '*.{js,jsx,ts,tsx,vue,html,json,md,css,scss}': ['prettier --write'],
   '*.{vue,css,scss}': ['stylelint --fix'],
