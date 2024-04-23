df06f934aed5fd545cb577cc3dbc2d1b12726160
skyclouds2001 <skyclouds2001@163.com>
2024-04-23 21:40:45 +0800

    style: rename folder name to *s

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import '@/styles/global.css'
 import '@/styles/tailwind.css'
 import App from '@/App.vue'
 import router from '@/router'
-import store from '@/store'
+import store from 'src/stores'
 
 createApp(App).use(router).use(store).mount('#app')
