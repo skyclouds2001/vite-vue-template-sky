1de6adbb6780a2227069c6eb6c6ab425dedb0e63
skyclouds2001 <skyclouds2001@163.com>
2024-09-15 21:17:35 +0800

    test: remove useless plugin

--- tests/components/index.spec.ts
+++ tests/components/index.spec.ts
@@ -1,14 +1,13 @@
 import { describe, expect, it } from 'vitest'
 import { render, screen } from '@testing-library/vue'
 import MainPage from '@/views/MainPage.vue'
-import router from '@/router'
 import store from '@/stores'
 
 describe('basic tests', () => {
   it('basic test', () => {
     render(MainPage, {
       global: {
-        plugins: [router, store],
+        plugins: [store],
       },
     })
     expect(screen.getByText('Main')).not.toBeNull()
