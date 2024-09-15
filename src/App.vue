b70eb793c5e9b1569f7f96ade603083764b9c8c6
skyclouds2001 <skyclouds2001@163.com>
2024-09-15 16:01:17 +0800

    feat: update basic vue component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ElConfigProvider } from 'element-plus'
+import { ElConfigProvider, ElIcon } from 'element-plus'
 // @ts-expect-error can't find type definition file
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import { Loading } from '@element-plus/icons-vue'
 import { locale } from 'dayjs'
 import 'dayjs/locale/zh-cn'
 
@@ -10,7 +11,22 @@ locale('zh-cn')
 
 <template>
   <el-config-provider :locale="zhCn">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <template v-if="Component">
+        <transition>
+          <keep-alive>
+            <suspense>
+              <component :is="Component" />
+              <template #fallback>
+                <el-icon>
+                  <Loading />
+                </el-icon>
+              </template>
+            </suspense>
+          </keep-alive>
+        </transition>
+      </template>
+    </router-view>
   </el-config-provider>
 </template>
 
