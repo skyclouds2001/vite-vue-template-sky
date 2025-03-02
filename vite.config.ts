1647d161ff81c8f28313a86ae166e8af26c4a4d4
skyclouds2001 <skyclouds2001@163.com>
2025-03-02 15:10:36 +0800

    feat: small update

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     legacy(),
-    ElementPlus({}),
+    ElementPlus(),
     pwa({
       registerType: 'autoUpdate',
       manifest: false,
